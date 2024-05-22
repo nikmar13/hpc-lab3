bfdc7589fd66a0ec36edbcf0d908c6f86e2bebdd
Mykyta Martsevyi <53223543+nikmar13@users.noreply.github.com>
2024-05-22 14:09:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,10 @@
 #include <math.h>
 #include <omp.h>
 
-// Генерація випадкових чисел в діапазоні [0, 1)
 double rand_double() {
     return rand() / (RAND_MAX + 1.0);
 }
 
-// Генерація випадкової матриці зв'язності
 void generate_adjacency_matrix(int N, double R, int **A) {
     #pragma omp parallel for
     for (int i = 0; i < N; i++) {
@@ -30,7 +28,6 @@ void generate_adjacency_matrix(int N, double R, int **A) {
     }
 }
 
-// Виведення матриці
 void print_matrix(int N, int **A) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -41,26 +38,22 @@ void print_matrix(int N, int **A) {
 }
 
 int main() {
-    int N = 10000;  // Розмірність сітки
-    double R = 100; // Радіус зв'язності
+    int N = 10000;
+    double R = 100;
 
-    // Виділення пам'яті для матриці
     int **A = (int **)malloc(N * sizeof(int *));
     for (int i = 0; i < N; i++) {
         A[i] = (int *)malloc(N * sizeof(int));
     }
 
-    // Генерація матриці зв'язності
     double start_time = omp_get_wtime();
     generate_adjacency_matrix(N, R, A);
     double end_time = omp_get_wtime();
 
     printf("Time taken: %f seconds\n", end_time - start_time);
 
-    // Виведення матриці (опціонально)
     // print_matrix(N, A);
 
-    // Звільнення пам'яті
     for (int i = 0; i < N; i++) {
         free(A[i]);
     }
